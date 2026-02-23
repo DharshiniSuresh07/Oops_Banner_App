d76a926a10e8c4f86c430a3b10306ab1665a6872
DharshiniSuresh07 <sdharshini1310@gmail.com>
2026-02-23 21:13:20 +0530

    UC2 - Added ASCII OOPS banner

---
+++ OOPSBannerApp.java
@@ -0,0 +1,14 @@
+public class OOPSBannerApp {
+
+    public static void main(String[] args) {
+
+        // Row 1
+        System.out.println("  *** " + "   " + "   *** " + "   " + " ****** " + "   " + " ****** ");
+        System.out.println("**   **" + "   " + "**   **" + "   " + "**    **" + "   " + "**      ");
+        System.out.println("**   **" + "   " + "**   **" + "   " + "**    **" + "   " + "**      ");
+        System.out.println("**   **" + "   " + "**   **" + "   " + "******  " + "   " + " *****  ");
+        System.out.println("**   **" + "   " + "**   **" + "   " + "**      " + "   " + "     ** ");
+		System.out.println("**   **" + "   " + "**   **" + "   " + "**      " + "   " + "     ** ");
+        System.out.println("  *** " + "   " + "   *** " + "   " + "**      " + "   " + " ****** ");
+    }
+}
